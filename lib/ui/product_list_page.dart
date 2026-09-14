2beef18d967b7e53e7ef69a8ffb6aa0ec1eab6d3
EdgyPotato <64056626+EdgyPotato@users.noreply.github.com>
2026-09-15 02:48:16 +0800

    Add simple UI for product list

---
+++ lib/ui/product_list_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '/data/product.dart';
+import '/data/product_api.dart';
+
+class ProductListPage extends StatefulWidget {
+  const ProductListPage({super.key});
+
+  @override
+  State<ProductListPage> createState() => _ProductListPageState();
+}
+
+class _ProductListPageState extends State<ProductListPage> {
+  List<Product> products = [];
+  bool isLoading = false;
+  String? errorMessage;
+
+  Future<void> _loadProducts() async {
+    setState(() {
+      isLoading = true;
+      errorMessage = null;
+    });
+
+    try {
+      final productResult = await ProductApi().fetchProducts(
+        limit: 20,
+        skip: 0,
+      );
+      setState(() {
+        products = productResult.products;
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Product List')),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return ListTile(
+            leading: Image.network(product.thumbnail),
+            title: Text(product.title),
+            subtitle: Text(product.price.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
